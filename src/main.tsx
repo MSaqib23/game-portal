48df95267fc4275717d317c79673229d48be0986
Muhammad Saqib Siddiqui <msaqib.23@gmail.com>
2024-02-27 01:28:37 +0500

    Build a dark mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ChakraProvider } from '@chakra-ui/react'
+import theme from './theme.ts'
 import App from './App.tsx'
 import 'bootstrap/dist/css/bootstrap.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
     <App />
     </ChakraProvider>
   </React.StrictMode>,
