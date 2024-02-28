39a3ddbee30ca56d37528e80e779916a04d07008
Muhammad Saqib Siddiqui <msaqib.23@gmail.com>
2024-02-28 23:42:33 +0500

    Displaying Critics Score

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,7 @@
-import { Card, CardBody, Heading, Image } from "@chakra-ui/react"
+import { Card, CardBody, HStack, Heading, Image } from "@chakra-ui/react"
 import { Game } from "../hooks/useGame"
 import PlatformIconList from "./PlatformIconList"
+import CriticScore from "./CriticScore"
 
 interface Prop{
     game: Game
@@ -11,7 +12,10 @@ const GameCard = ({game}:Prop) => {
         <Image src={game.background_image}/>
         <CardBody >
             <Heading fontSize='xl'>{game.name}</Heading>
+            <HStack justifyContent='space-between'>
             <PlatformIconList platforms={game.parent_platforms.map(p=>p.platform)}/>
+            <CriticScore score={game.metacritic}/>
+            </HStack>
         </CardBody>
     </Card>
   )
