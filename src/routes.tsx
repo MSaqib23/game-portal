1c6b5b25364b8622fb96619b315c6078aa324545
Muhammad Saqib Siddiqui <msaqib.23@gmail.com>
2024-03-14 14:18:04 +0400

    Handle Errors

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,11 +2,13 @@ import { createBrowserRouter } from "react-router-dom";
 import Layout from "./services/pages/Layout";
 import HomePage from "./services/pages/HomePage";
 import GameDetailPage from "./services/pages/GameDetailPage";
+import ErrorPage from "./services/pages/ErrorPage";
 
 const router=createBrowserRouter([
     {
         path: '/',
         element: <Layout />,
+        errorElement:<ErrorPage/>,
         children: [
             {
                 index: true,
